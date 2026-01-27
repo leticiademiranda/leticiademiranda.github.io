7f53f6ca2aafd5b6d66cac0b5d5f9bff9ffc1b98
leticiademiranda <demirandabio@gmail.com>
2026-01-27 17:55:02 +0000

    Deploying to gh-pages from @ leticiademiranda/leticiademiranda.github.io@657a1b7e852ea1b95622bdd1643ac2ac3a26a795 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-letícia-de-miranda",
+    title: "Letícia de Miranda",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
